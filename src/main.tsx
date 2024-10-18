8513594da59fc4881404c1c31b1be0e5e67b22d4
code1426 <gmver27@gmail.com>
2024-10-18 21:49:04 +0800

    Initialized the repository

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div className='text-5xl text-blue-700 items-center justify-center flex h-screen bg-slate-300'>
+      ICAST
+      </div>
+  </StrictMode>
+)
