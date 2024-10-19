135a26ada6ab1e71d8a47d0014ea5d3e832b1088
Rops-11 <jrofer.casio11@gmail.com>
2024-10-19 16:21:49 +0800

    Renaming of Files to PascalCase

---
+++ src/pages/Encoder.tsx
@@ -0,0 +1,12 @@
+// src/icastencoder.tsx
+import React from 'react';
+
+const IcastEncoder = () => {
+  return (
+    <div className='flex justify-center items-center h-screen bg-gray-200'>
+      <h1 className='text-4xl font-bold'>Welcome to IcastEncoder Page</h1>
+    </div>
+  );
+};
+
+export default IcastEncoder;
