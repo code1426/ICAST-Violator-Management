a97e26535435e0734022933a482818df333034b7
Rops-11 <jrofer.casio11@gmail.com>
2024-10-31 21:34:24 +0800

    Violator List small adjustments and Header Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        'beige-color' : '#D0B49F',
+        'beige-color': '#D0B49F',
       },
     },
   },
   plugins: [],
-}
+};
